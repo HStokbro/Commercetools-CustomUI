2e1f394aeb63978c93c0e6fd747167121fea53ed
Henrik Stokbro <hstokbro@gmail.com>
2021-02-04 15:16:21 +0100

    Fix startup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EntryPoint from './components/entry-point';
 
+// Cannot rename the file to .jsx as mc-script is hardcoded to .js
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<EntryPoint />, document.getElementById('app'));
