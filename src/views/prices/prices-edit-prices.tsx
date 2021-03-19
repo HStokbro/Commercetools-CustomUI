b770bb1f0e4011eda29695950704a32cebc730dc
Henrik Stokbro <hstokbro@gmail.com>
2021-03-19 12:09:35 +0100

    Add pricing edit WIP

--- src/views/prices/prices-edit-prices.tsx
+++ src/views/prices/prices-edit-prices.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
-import { GetVariantsQuery, useGetVariantsLazyQuery } from '../../generated/graphql';
-import { GQLQueryOptions } from '../../constants';
+import React, { useEffect, useState } from 'react';
+import Text from '@commercetools-uikit/text';
+import MoneyField from '@commercetools-uikit/money-field';
+import Card from '@commercetools-uikit/card';
+import { useGetCurrenciesQuery, useGetVariantsLazyQuery } from '../../generated/graphql';
+import { GQLContext, GQLQueryOptions, GQLCurrentLocale } from '../../utils/gqlHelpers';
 import { ListProduct } from '../../types';
 
 type Props = {
@@ -9,31 +12,131 @@ type Props = {
 };
 
 const PricesEditPrices = (props: Props): JSX.Element => {
-  const allListProducts = [props.masterProduct, ...props.references];
-  const where = allListProducts.map((r) => `id="${r.id}"`).join(' or ');
-  const [getAllVariants, variantState] = useGetVariantsLazyQuery({
-    variables: {
-      where,
-    },
-    ...GQLQueryOptions,
-  });
+  const readyToLoad = props.references.length;
+  const locale = GQLCurrentLocale();
 
+  const currenciesState = useGetCurrenciesQuery(GQLContext);
+
+  const [getMaster, masterState] = useGetVariantsLazyQuery(GQLQueryOptions);
   useEffect(() => {
-    getAllVariants();
+    if (readyToLoad) {
+      getMaster({
+        variables: {
+          locale,
+          where: `id="${props.masterProduct.id}"`,
+        },
+      });
+    }
   }, [props.references]);
 
-  if (variantState.error) {
-    return <>{`Error! ${variantState.error?.message || ''}`}</>;
+  const [getReferences, referencesState] = useGetVariantsLazyQuery(GQLQueryOptions);
+  useEffect(() => {
+    if (readyToLoad) {
+      const where = props.references.map((r) => `id="${r.id}"`).join(' or ');
+      getReferences({
+        variables: {
+          locale,
+          where,
+        },
+      });
+    }
+  }, [props.references]);
+
+  if (masterState.error || referencesState.error) {
+    return <>{`Error! ${masterState.error?.message || ''} ${referencesState.error?.message || ''}`}</>;
   }
-  if (variantState.loading || !variantState.data) {
+  if (masterState.loading || referencesState.loading) {
     return <>Loading...</>;
   }
 
-  const getVariantsArray = (data?: GetVariantsQuery) =>
-    data?.products.results.map((result) => result.masterData.current.allVariants);
-  const variantsArray = getVariantsArray(variantState.data);
+  console.log(masterState.data, referencesState.data);
 
-  return <>{variantsArray.map((variants) => variants.map((variant) => <p key={variant.key}>{variant.key}</p>))}</>;
+  if (!props.masterProduct || !props.references.length || !masterState.data || !referencesState.data) return null;
+
+  const { currencies } = currenciesState.data.project;
+  const master = masterState.data.products.results[0];
+  const references = referencesState.data.products.results;
+  const masterVariants = master.masterData.current.allVariants;
+
+  const buildVariantReference = (item) => {
+    const { id, key } = item;
+    return { id, key };
+  };
+
+  const buildProductReference = (item) => {
+    const { id } = item;
+    const { name } = item.masterData.current;
+    return { id, name };
+  };
+
+  const buildVariantPrices = (masterVariant) => {
+    const reference = references[0];
+    const referenceVariants = reference.masterData.current.allVariants;
+
+    const referencePrices = referenceVariants.map((variant) => {
+      return {
+        price: { amount: '', currencyCode: currencies[0] },
+        references: [
+          {
+            product: buildProductReference(reference),
+            variant: buildVariantReference(variant),
+          },
+        ],
+      };
+    });
+
+    return {
+      variant: buildVariantReference(masterVariant),
+      referencePrices,
+    };
+  };
+
+  const priceObject = {
+    product: buildProductReference(props.masterProduct),
+    references: references.map((r) => buildProductReference(r)),
+    variantPrices: masterVariants.map((masterVariant) => buildVariantPrices(masterVariant)),
+  };
+
+  console.log('prices', priceObject, masterState);
+
+  return (
+    <>
+      {priceObject.variantPrices.map((variantPrice, variantPriceIndex) => (
+        <div key={variantPrice.variant.id}>
+          <Text.Headline as="h2">{variantPrice.variant.key}</Text.Headline>
+          {variantPrice.referencePrices.map((referencePrice, referencePriceIndex) => {
+            const key = referencePrice.references.map((ref) => `${ref.product.id}-${ref.variant.id}`).join('+');
+            const label = referencePrice.references
+              .map((ref) => `${ref.product.name} - ${ref.variant.key}`)
+              .join(' + ');
+            return (
+              <div key={key}>
+                <MoneyField
+                  title={label}
+                  value={referencePrice.price}
+                  onChange={(event) => {
+                    console.log(
+                      priceObject.variantPrices[variantPriceIndex].referencePrices[referencePriceIndex].price.amount,
+                      event.target.value,
+                    );
+
+                    priceObject.variantPrices[variantPriceIndex].referencePrices[referencePriceIndex].price.amount =
+                      event.target.value;
+
+                    console.log(
+                      priceObject.variantPrices[variantPriceIndex].referencePrices[referencePriceIndex].price.amount,
+                      event.target.value,
+                    );
+                  }}
+                  currencies={currencies}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </>
+  );
 };
 
 PricesEditPrices.displayName = 'PricesEditPrices';
