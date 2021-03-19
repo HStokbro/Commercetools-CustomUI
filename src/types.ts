e6045f5b2d01102d20138afc918d1bceb695394d
Henrik Stokbro <hstokbro@gmail.com>
2021-03-19 15:44:23 +0100

    Add types for Redux state
    Add types for references
    Add types for ProductType, Category, State

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,86 @@
-import { Maybe, Product, ProductData, ProductTypeDefinition } from './generated/graphql';
+import {
+  GetCategoriesQuery,
+  GetProjectQuery,
+  GetProductsQuery,
+  GetProductTypeDefinitionsQuery,
+  Maybe,
+  Product,
+  ProductData,
+  ProductTypeDefinition,
+  Category,
+} from './generated/graphql';
 
+// Copied from GetProductsQuery results in graphql.tsx
 export type ListProduct = Pick<Product, 'id' | 'version'> & {
   productType?: Maybe<Pick<ProductTypeDefinition, 'id' | 'key'>>;
   masterData: { current?: Maybe<Pick<ProductData, 'name'>> };
 };
 
-export type ListProductSelection = ListProduct & { checkbox: boolean };
+// Copied from GetProductTypeDefinitionsQuery results in graphql.tsx
+export type ListProductTypeDefinition = Pick<ProductTypeDefinition, 'key' | 'name' | 'description' | 'id'>;
+
+// Copied from GetCategoriesQuery results in graphql.tsx
+export type ListCategory = Pick<Category, 'id' | 'name' | 'description'> & {
+  ancestors: Array<Pick<Category, 'id' | 'name'>>;
+};
+
+// Use as type in UIKit DataTable for selection
+// Example: WithSelection<ListProduct>
+export type WithSelection<T> = T & { checkbox: boolean };
+
+export enum State {
+  INITIAL = 'INITIAL',
+  LOADING = 'LOADING',
+  SAVING = 'SAVING',
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+}
+
+export enum ReferenceType {
+  SUBSCRIPTION = 'SUBSCRIPTION',
+  PAYMENT = 'PAYMENT',
+  UPSELLINBUNDLE = 'UPSELLINGROUP',
+  UPSELL = 'UPSELL',
+}
+
+export enum ReferenceLinkType {
+  PRODUCT = 'PRODUCT',
+  PRODUCTTYPE = 'PRODUCTTYPE',
+  CATEGORY = 'CATEGORY',
+}
+
+export type ReferenceProduct = {
+  id: string;
+  variantIds?: string[];
+};
+
+export type ReferenceLink = {
+  ids: string[];
+} & (
+  | { linkType: ReferenceLinkType.PRODUCT; variantIds?: string[] }
+  | {
+      linkType: Exclude<ReferenceLinkType, ReferenceLinkType.PRODUCT>;
+    }
+);
+
+export type Reference = {
+  variantId?: string;
+  type: ReferenceType;
+  links: ReferenceLink[];
+};
+
+// export type Price = {
+//   references: Reference[];
+// };
+
+export type ReduxState = {
+  state: State;
+  project: GetProjectQuery;
+
+  products: GetProductsQuery;
+  productTypes: GetProductTypeDefinitionsQuery;
+  categories: GetCategoriesQuery;
+
+  references: Reference[];
+  // prices: Price[];
+};
